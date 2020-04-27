b72632774cbd8430909f90b10555c056ce1581df
krushcev <ronintoadin@gmail.com>
2020-04-27 14:12:38 +0700

    try

---
+++ block.js
@@ -0,0 +1,38 @@
+const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
+
+class Block {
+    constructor({timestamp, lastHash, hash, data}) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    static genesis() {
+        return new this(GENESIS_DATA);
+    }
+
+    static mineBlock({lastBlock, data}) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
+        return new this({
+            timestamp,
+            lastHash,
+            data, 
+            hash: cryptoHash(timestamp, lastHash, data)
+        });
+    }
+}
+
+// const block1 = new Block({
+//     timestamp:'01/01/01',
+//     lastHash:'foo-lastHash',
+//     data:'foo-data',
+//     hash:'foo-hash',
+// });
+
+// console.log('block1',block1);
+
+module.exports = Block;
