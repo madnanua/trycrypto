ba1c453b48088ca4211885320e5f9f7287c53118
madnanua <ronintoadin@gmail.com>
2021-11-28 13:10:03 +0700

    cleanify

---
+++ notes/note.js
@@ -0,0 +1,89 @@
+{
+    id: 13826529,
+        name: "",
+            number: "Yogyakarta - Cirebon 21.00",
+                service_name: "Citra Adi Lancar",
+                    origin_id: 193479,
+                        destination_id: 193481,
+                            op_schedule_id: 47,
+                                travel_date: "2021-09-29",
+                                    travel_id: 254,
+                                        travels_name: "Citra Adi Lancar",
+                                            route_id: 4,
+                                                route_map_id: null,
+                                                    available_seats: 40,
+                                                        description: "",
+                                                            dep_time: "21:00",
+                                                                duration: "09:00",
+                                                                    arr_time: "06:00",
+                                                                        bus_type: "2+2, VIP, AC, Video",
+                                                                            cost: "SS:150000.0",
+                                                                                can_cancel: true,
+                                                                                    cancellation_time: 0,
+                                                                                        cabin_layout: false,
+                                                                                            status: "New",
+                                                                                                is_service_tax_applicable: false,
+                                                                                                    helpline_number: null,
+                                                                                                        amenities: "",
+                                                                                                            via: "Purwokerto",
+                                                                                                                bus_layout:
+    {
+        total_seats: 40,
+            coach_details: "ST_IMG|.GY-|.GY-|.GY-|.GY-DR_IMG|.GY,1A|SS-1B|SS-|.GY-1C|SS-1D|SS,2A|SS-2B|SS-|.GY-2C|SS-2D|SS,3A|SS-3B|SS-|.GY-3C|SS-3D|SS,4A|SS-4B|SS-|.GY-4C|SS-4D|SS,5A|SS-5B|SS-|.GY-5C|SS-5D|SS,6A|SS-6B|SS-|.GY-6C|SS-6D|SS,7A|SS-7B|SS-|.GY-7C|SS-7D|SS,8A|SS-8B|SS-|.GY-8C|SS-8D|SS,9A|SS-9B|SS-|.GY-9C|SS-9D|SS,ST_IMG|.GY-|.GY-|.GY-10C|SS-10D|SS,WR_IMG|.GY-WR_IMG|.GY-|.GY-11C|SS-11D|SS",
+                available: "1A|150000.0,1B|150000.0,1C|150000.0,1D|150000.0,2A|150000.0,2B|150000.0,2C|150000.0,2D|150000.0,3A|150000.0,3B|150000.0,3C|150000.0,3D|150000.0,4A|150000.0,4B|150000.0,4C|150000.0,4D|150000.0,5A|150000.0,5B|150000.0,5C|150000.0,5D|150000.0,6A|150000.0,6B|150000.0,6C|150000.0,6D|150000.0,7A|150000.0,7B|150000.0,7C|150000.0,7D|150000.0,8A|150000.0,8B|150000.0,8C|150000.0,8D|150000.0,9A|150000.0,9B|150000.0,9C|150000.0,9D|150000.0,10C|150000.0,10D|150000.0,11C|150000.0,11D|150000.0",
+                    ladies_seats: "",
+                        gents_seats: "",
+                            ladies_booked_seats: "",
+                                gents_booked_seats: "",
+                                    allow_gents_next_to_ladies: true,
+                                        allow_ladies_next_to_gents: false,
+                                            boarding_stages: "221325|21:00|Jl. Jogja - Wonosari No.Km.6, Kalangan, Baturetno, Kec. Banguntapan, Bantul, Daerah Istimewa Yogyakarta 55197, Indonesia|Garasi Citra Adi Lancar|0274443101|Garasi Citra Adi Lancar|1|221325|27|20",
+                                                dropoff_stages: "204860|06:00|Terminal Harjamukti Cirebon, Jl. Ahmad Yani, Kecapi, Harjamukti, Cirebon City, West Java 45142, Indonesia|Terminal Harjamukti Cirebon|0|Agen Terminal Harjamukti|2|204860|28|14",
+                                                    floor: "",
+                                                        last_seats: "",
+                                                            forced_seats: "",
+                                                                fares_hash:
+        {
+            SS:
+            {
+                Adult: "150000"
+            }
+        }
+    },
+    trip_id: "BITLA:254:4",
+        city_seq_order:
+    [
+        "193479",
+        "193510",
+        "193496",
+        "193481",
+        "193480",
+        "193473",
+        "194407"
+    ],
+        cancellation_policy: "",
+            main_dep_time: "29/09/2021 21:00",
+                service_tax_percent: null,
+                    convenience_charge_percent: 0,
+                        is_ac_bus: true,
+                            api_type: 1,
+                                allow_reschedule: true,
+                                    route_version: "13826529-2",
+                                        flexi_fare: null,
+                                            allow_ladies_to_book_double_seats: true,
+                                                is_seperate_service_tax_allowed: false,
+                                                    res_details: { },
+    transaction_charges: "",
+        no_coach_layout: false,
+            max_child_age: null,
+                min_senior_age: null,
+                    passenger_category_hash:
+    {
+        Adult: "ADULT"
+    },
+    is_meal_allowed: false,
+        meal_count: 0,
+            available_meal_types: [],
+                social_distancing_guaranteed: false,
+                    virtual_stage_ids: []
+}
